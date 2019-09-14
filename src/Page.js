2b8c82a67a4e1cd4f480012715097fcf48da3356
Steven Villa <svill069@fiu.edu>
2019-09-14 17:30:49 -0400

    created new layout. Missing social div and new youtube api

--- src/Page.js
+++ src/Page.js
@@ -7,16 +7,29 @@ class Live extends Component {
       <div>
         <AnimatedWaves>
           <div className="live-container">
-            <div className="messages-container">
-              <div>
-                <img
+            <div className="left-container">
+            <img
                   className="main-section-details-logo"
                   alt="ShellHacks Wordmark"
                   src={require("./assets/shellhacks.svg")}
                 />
-                <h1>Live Page</h1>
-                <h2>Live message array goes here</h2>
-                {/* <LiveMessage/> */}
+              <div className="devpost-tables-container">
+                <div className="devpost">
+                <a href="https://devpost.com/">
+                  <img src="https://marketing-challengepost.netdna-ssl.com/assets/reimagine2/devpost-logo-a0cd0d3c3681a858b200141ed18a9bf9.svg"/></a>
+                </div>
+                <a href="#"><h2>Tables</h2></a>
+              </div>
+              <div className="mlh-workshops">
+                <div >
+                  <a href="https://hardware.mlh.io/">
+                    <img className="mlh-hardware"src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Major_League_Hacking_logo.svg/375px-Major_League_Hacking_logo.svg.png"/>
+                  </a>
+                </div>
+                <a href="#"><h2>Workshop Stream</h2></a>
+              </div>
+              <div className="socials">
+  
               </div>
               <div className="music-player-section">
                 {/* <iframe src="https://open.spotify.com/embed/playlist/2R0OPkOjSI3AzpnC0VCbs5" width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
@@ -31,20 +44,44 @@ class Live extends Component {
                 ></iframe>
               </div>
             </div>
-            <div className="schedule-continer">
-              <h2>Schedule:</h2>
-              <div className="live-schedule">
-                {liveSchedule.map((card, i) => {
-                  let props = {
-                    title: card.title,
-                    description: card.description,
-                    styleClassName: "live-card",
-                    activeClassName: "active-live-card",
-                    descStyle: "active-live-card card-content p",
-                    i: i
-                  };
-                  return <Card key={i} {...props} />;
-                })}
+            <div className="right-container">
+              <div className="timer-container">
+                <div>Timer goes here</div>
+                <div>Current time here</div>
+              </div>
+              <div className="schedule-and-announcements">
+                <div className="anouncements-container">
+                  <h2>Announcements</h2>
+                  <div className="announcement-list">
+                  {liveSchedule.map((card, i) => {
+                      let props = {
+                        title: card.title,
+                        description: card.description,
+                        styleClassName: "live-card",
+                        activeClassName: "active-live-card",
+                        descStyle: "active-live-card card-content p",
+                        i: i
+                      };
+                      return <Card key={i} {...props} />;
+                    })}
+                  </div>
+                </div>
+                <div className="schedule-container">
+                  <h2>Schedule</h2>
+                  <div className="live-schedule">
+                    {liveSchedule.map((card, i) => {
+                      let props = {
+                        title: card.title,
+                        description: card.description,
+                        styleClassName: "live-card",
+                        activeClassName: "active-live-card",
+                        descStyle: "active-live-card card-content p",
+                        i: i
+                      };
+                      return <Card key={i} {...props} />;
+                    })}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
