0a72f614f870fb6bc3237df1aa80a68669333651
Steven Villa <svill069@fiu.edu>
2019-09-16 23:13:34 -0400

    made new card and card list

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,7 +2,7 @@
  * Reusable card component
  */
 
-import React, { Component } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faPlus,
@@ -11,71 +11,18 @@ import {
 import PropTypes from 'prop-types'
 import './styles.css'
 
-class Card extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      clicked: false,
+const Card = (title, startTime, location, organizer, description)=> (
+  <div className="card-container">
+    <h1>{title}</h1>
+    <h3>{startTime}</h3>
+    <h3>{location}</h3>
+    <h3>{organizer}</h3>
+    <p>{description}</p>
+  </div>
+)
 
-    }
-  }
+const CardList = ()=> {
 
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked })
-  }
-
-  render () {
-    let {
-      title,
-      description,
-      date,
-      content,
-      styleClassName,
-      activeClassName,
-      titleIcon,
-      descStyles
-    } = this.props
-    let cardContent = description ? (
-      <p style={descStyles}>{description}</p>
-    ) : (
-      content
-    )
-    let currenStyle = this.state.clicked ? activeClassName : styleClassName
-
-    return (
-      <div className='card-container'>
-        <div
-          className={'card-button ' + currenStyle}
-          onClick={this.handleClick}
-        >
-          <div style={{display:"flex"}}> 
-           {titleIcon && <FontAwesomeIcon icon={titleIcon} />}
-          <div>
-          <h3>{title}</h3>
-          <p>{date}</p>
-          </div>
-          </div>
-        <div>{this.state.clicked? <FontAwesomeIcon icon={faMinus} /> : <FontAwesomeIcon icon={faPlus} />}</div> 
-        </div>
-        <div
-          className='card-content'
-          style={this.state.clicked ? { display: 'flex' } : { display: 'none' }}
-        >
-          {cardContent}
-        </div>
-
-      </div>
-    )
-  }
 }
 
-Card.propTypes = {
-  title: PropTypes.string.isRequired,
-  styleClassName: PropTypes.string.isRequired,
-  activeClassName: PropTypes.string.isRequired,
-  description: PropTypes.string,
-  content: PropTypes.elementType,
-  date: PropTypes.string
-}
-
-export { Card }
+export { Card, CardList }
