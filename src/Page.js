c49f2891e961f717f0be95c73cde9d6da9689f7f
Steven Villa <svill069@fiu.edu>
2019-09-16 20:19:21 -0400

    added social media container

--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import { Card, AnimatedWaves, liveSchedule } from "./components";
+import {
+  faFacebookSquare,
+  faInstagram,
+  faLinkedin,
+  faTwitterSquare,
+  faDiscord
+} from "@fortawesome/free-brands-svg-icons"; //need to pre-load in app.js
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles.css";
 class Live extends Component {
   render() {
@@ -8,28 +16,42 @@ class Live extends Component {
         <AnimatedWaves>
           <div className="live-container">
             <div className="left-container">
-            <img
-                  className="main-section-details-logo"
-                  alt="ShellHacks Wordmark"
-                  src={require("./assets/shellhacks.svg")}
-                />
+              <img
+                className="main-section-details-logo"
+                alt="ShellHacks Wordmark"
+                src={require("./assets/shellhacks.svg")}
+              />
               <div className="devpost-tables-container">
                 <div className="devpost">
-                <a href="https://devpost.com/">
-                  <img src="https://marketing-challengepost.netdna-ssl.com/assets/reimagine2/devpost-logo-a0cd0d3c3681a858b200141ed18a9bf9.svg"/></a>
+                  <a href="https://devpost.com/">
+                    <img src="https://marketing-challengepost.netdna-ssl.com/assets/reimagine2/devpost-logo-a0cd0d3c3681a858b200141ed18a9bf9.svg" /></a>
                 </div>
                 <a href="#"><h2>Tables</h2></a>
               </div>
               <div className="mlh-workshops">
                 <div >
                   <a href="https://hardware.mlh.io/">
-                    <img className="mlh-hardware"src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Major_League_Hacking_logo.svg/375px-Major_League_Hacking_logo.svg.png"/>
+                    <img className="mlh-hardware" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Major_League_Hacking_logo.svg/375px-Major_League_Hacking_logo.svg.png" />
                   </a>
                 </div>
                 <a href="#"><h2>Workshop Stream</h2></a>
               </div>
-              <div className="socials">
-  
+              <div className="social-media-container">
+                <a href="https://discordapp.com/invite/upefiu">
+                  <FontAwesomeIcon icon={faDiscord} size="4x" />
+                </a>
+                <a href="https://www.instagram.com/upefiu/">
+                  <FontAwesomeIcon icon={faInstagram} size="4x" />
+                </a>
+                <a href="https://www.facebook.com/upefiu">
+                  <FontAwesomeIcon icon={faFacebookSquare} size="4x" />
+                </a>
+                <a href="https://twitter.com/upefiu">
+                  <FontAwesomeIcon icon={faTwitterSquare} size="4x" />
+                </a>
+                <a href="https://www.linkedin.com/company/upe-fiu/">
+                  <FontAwesomeIcon icon={faLinkedin} size="4x" />
+                </a>
               </div>
               <div className="music-player-section">
                 {/* <iframe src="https://open.spotify.com/embed/playlist/2R0OPkOjSI3AzpnC0VCbs5" width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
@@ -53,7 +75,7 @@ class Live extends Component {
                 <div className="anouncements-container">
                   <h2>Announcements</h2>
                   <div className="announcement-list">
-                  {liveSchedule.map((card, i) => {
+                    {liveSchedule.map((card, i) => {
                       let props = {
                         title: card.title,
                         description: card.description,
