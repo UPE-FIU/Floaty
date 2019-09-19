197357f2c5c5dd0d5ea49c349710ef54d6854382
jehf denezaire <jehf.denezaire@gmail.com>
2019-09-19 03:16:39 -0400

    fixing conflict

--- src/Page.js
+++ src/Page.js
@@ -9,82 +9,83 @@ import {
 } from "@fortawesome/free-brands-svg-icons"; //need to pre-load in app.js
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LinkContiner from "./components/LinkGroup";
-import {CurrentTime, CountdownTimer} from "./components/Timer"
+import { CurrentTime, CountdownTimer } from "./components/Timer";
 import data from "./dummy";
 import data2 from "./dummy2";
 import List from "./components/List";
 import Card from "./components/Card";
-import sockets from "./services/sockets";
+import styles from "./scss/Page.module.scss";
 
 class Live extends Component {
   render() {
     return (
-        <AnimatedWaves>
-          <div className="live-container">
-            {
-
-
-
-
-
-            }
-            <div className="left-container">
-              <img
-                className="main-section-details-logo"
-                alt="ShellHacks Wordmark"
-                src={require("./assets/shellhacks.svg")}
-              />
-              <LinkContiner
-                firstLinkDest="https://devpost.com/"
-                imgSrc="https://marketing-challengepost.netdna-ssl.com/assets/reimagine2/devpost-logo-a0cd0d3c3681a858b200141ed18a9bf9.svg"
-                secondLinkName="Tables"
-                secondLinkDest="#"
-              />
-              <LinkContiner
-                link="https://hardware.mlh.io/"
-                imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Major_League_Hacking_logo.svg/375px-Major_League_Hacking_logo.svg.png"
-                secondLinkName="Workshop Stream"
-                secondLinkDest="#"
-              />
-              <div className={styles.socialMediaContainer}>
-                <a className={styles.icon} href="https://discordapp.com/invite/upefiu">
-                  <FontAwesomeIcon icon={faDiscord} size="4x" />
-                </a>
-                <a className={styles.icon} href="https://www.instagram.com/upefiu/">
-                  <FontAwesomeIcon icon={faInstagram} size="4x" />
-                </a>
-                <a className={styles.icon} href="https://www.facebook.com/upefiu">
-                  <FontAwesomeIcon icon={faFacebookSquare} size="4x" />
-                </a>
-                <a className={styles.icon} href="https://twitter.com/upefiu">
-                  <FontAwesomeIcon icon={faTwitterSquare} size="4x" />
-                </a>
-                <a
-                  className={styles.icon}
-                  href="https://www.linkedin.com/company/upe-fiu/"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} size="4x" />
-                </a>
-              </div>
-              <div className={styles.musicPlayerSection}>
-                {/* <iframe src="https://open.spotify.com/embed/playlist/2R0OPkOjSI3AzpnC0VCbs5" width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
-                <iframe
-                  title="YouTube Stream"
-                  width="400"
-                  height="200"
-                  src="https://www.youtube.com/embed/videoseries?list=PLhjbtbG8AMAR-wzrlksDY6eF1qEg-Cae0"
-                  frameborder="0"
-                  allow="autoplay; encrypted-media"
-                  allowfullscreen
-                ></iframe>
+      <AnimatedWaves>
+        <div className={styles.liveContainer}>
+          <div className={styles.leftContainer}>
+            <img
+              className="main-section-details-logo"
+              alt="ShellHacks Wordmark"
+              src={require("./assets/shellhacks.svg")}
+            />
+            <LinkContiner
+              firstLinkDest="https://devpost.com/"
+              imgSrc="https://marketing-challengepost.netdna-ssl.com/assets/reimagine2/devpost-logo-a0cd0d3c3681a858b200141ed18a9bf9.svg"
+              secondLinkName="Tables"
+              secondLinkDest="#"
+            />
+            <LinkContiner
+              link="https://hardware.mlh.io/"
+              imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Major_League_Hacking_logo.svg/375px-Major_League_Hacking_logo.svg.png"
+              secondLinkName="Workshop Stream"
+              secondLinkDest="#"
+            />
+            <div className={styles.socialMediaContainer}>
+              <a
+                className={styles.icon}
+                href="https://discordapp.com/invite/upefiu"
+              >
+                <FontAwesomeIcon icon={faDiscord} size="4x" />
+              </a>
+              <a
+                className={styles.icon}
+                href="https://www.instagram.com/upefiu/"
+              >
+                <FontAwesomeIcon icon={faInstagram} size="4x" />
+              </a>
+              <a className={styles.icon} href="https://www.facebook.com/upefiu">
+                <FontAwesomeIcon icon={faFacebookSquare} size="4x" />
+              </a>
+              <a className={styles.icon} href="https://twitter.com/upefiu">
+                <FontAwesomeIcon icon={faTwitterSquare} size="4x" />
+              </a>
+              <a
+                className={styles.icon}
+                href="https://www.linkedin.com/company/upe-fiu/"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="4x" />
+              </a>
+            </div>
+            <div className={styles.musicPlayerSection}>
+              {/* <iframe src="https://open.spotify.com/embed/playlist/2R0OPkOjSI3AzpnC0VCbs5" width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
+              <iframe
+                title="YouTube Stream"
+                width="400"
+                height="200"
+                src="https://www.youtube.com/embed/videoseries?list=PLhjbtbG8AMAR-wzrlksDY6eF1qEg-Cae0"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+          <div className={styles.rightContainer}>
+            <div className={styles.timerContainer}>
+              <CountdownTimer />
+              <div>
+                <CurrentTime />
               </div>
             </div>
-            <div className={styles.rightContainer}>
-               <div className={styles.timerContainer}>
-                  <CountdownTimer/>
-                <div><CurrentTime/></div>
-              </div>
-              <div className={styles.scheduleAndAnnouncements}>
+            <div className={styles.scheduleAndAnnouncements}>
               <List>
                 {data.map((card, i) => (
                   <Card key={i}>
@@ -106,8 +107,8 @@ class Live extends Component {
                   </Card>
                 ))}
               </List>
-              </div>
-                {/*
+            </div>
+            {/*
                 <div className="anouncements-container">
                   <h2>Announcements</h2>
                   <div className="announcement-list">
@@ -127,9 +128,9 @@ class Live extends Component {
                   </div>
                 </div>
               </div> */}
-            </div>
           </div>
-        </AnimatedWaves>
+        </div>
+      </AnimatedWaves>
     );
   }
 }
