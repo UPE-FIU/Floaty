e6ce9a75642fa1c7db91e4df6f773fecc5aa3da1
Steven Villa <svill069@fiu.edu>
2019-09-20 15:19:57 -0400

    fixed resources section

--- src/components/Demo/index.jsx
+++ src/components/Demo/index.jsx
@@ -13,12 +13,14 @@ const Demo = () => {
   return (
     <div className={styles.container}>
       <h3>Resources</h3>
-      <div>
+      <div className={styles.linkContainer}>
         <Link to="http://go.fiu.edu/SH19DevPost" id="demo-link">
-          <FontAwesomeIcon icon={faDiscord} size="4x" />
+          <img id={styles.devpostImage} style={{width: "100%"}} src="https://devpost-challengepost.netdna-ssl.com/assets/reimagine2/devpost-logo-646bdf6ac6663230947a952f8d354cad.svg" alt="Devpost"/>
         </Link>
         <Link to="https://devpost.com/" id="demo-link">
-          <FontAwesomeIcon icon={faDiscord} size="4x" />
+          <div className={styles.button}>
+            <h2>Table Assignment</h2>
+          </div>
         </Link>
       </div>
     </div>
